152ff4f3c16344b6dcd2fcdc6d1c5e64db3657a1
NachoGz <ignaciogomezf1@gmail.com>
2025-03-06 09:58:47 -0300

    add count players query

--- sql/queries/players.sql
+++ sql/queries/players.sql
@@ -2,3 +2,6 @@
 INSERT INTO players (id, name, turn, game_id, game_state_id, host)
 VALUES ($1, $2, $3, $4, $5, $6)
 RETURNING *;
+
+-- name: CountPlayers :one
+SELECT COUNT(*) FROM players WHERE game_id = $1;
