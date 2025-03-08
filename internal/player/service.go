60b3cd16b6b0b85c586523486dc44d3553f33276
NachoGz <ignaciogomezf1@gmail.com>
2025-03-08 11:57:19 -0300

    add CreatePlayer service function

--- internal/player/service.go
+++ internal/player/service.go
@@ -1,5 +1,13 @@
 package player
 
+import (
+	"context"
+	"database/sql"
+
+	"github.com/NachoGz/switcher-backend-go/internal/database"
+	"github.com/google/uuid"
+)
+
 // Service handles all game-related operations
 type Service struct {
 	playerRepo PlayerRepository
@@ -12,4 +20,22 @@ func NewService(
 	return &Service{
 		playerRepo: playerRepo,
 	}
+}
+
+func (s *Service) CreatePlayer(ctx context.Context, playerData Player) (*Player, error) {
+	player, err := s.playerRepo.CreatePlayer(ctx, database.CreatePlayerParams{
+		ID:          playerData.ID,
+		Name:        playerData.Name,
+		Turn:        sql.NullString{String: playerData.Turn},
+		GameID:      uuid.NullUUID{UUID: playerData.GameID},
+		GameStateID: uuid.NullUUID{UUID: playerData.GameStateID},
+		Host:        playerData.Host,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	resultPlayer := s.DBToModel(ctx, player)
+
+	return &resultPlayer, nil
 }
