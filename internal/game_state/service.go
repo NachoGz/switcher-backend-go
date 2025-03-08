45e432a03aee4566d8688c8ba18c3938e41fa95d
NachoGz <ignaciogomezf1@gmail.com>
2025-03-08 11:57:54 -0300

    add CreateGameState service function

--- internal/game_state/service.go
+++ internal/game_state/service.go
@@ -1,5 +1,12 @@
 package gameState
 
+import (
+	"context"
+
+	"github.com/NachoGz/switcher-backend-go/internal/database"
+	"github.com/google/uuid"
+)
+
 // Service handles all game-related operations
 type Service struct {
 	gameStateRepo GameStateRepository
@@ -12,4 +19,21 @@ func NewService(
 	return &Service{
 		gameStateRepo: gameStateRepo,
 	}
+}
+
+func (s *Service) CreateGameState(ctx context.Context, gameStateData GameState) (*GameState, error) {
+	gameState, err := s.gameStateRepo.CreateGameState(ctx, database.CreateGameStateParams{
+		ID:              gameStateData.ID,
+		State:           gameStateData.State,
+		GameID:          uuid.NullUUID{UUID: gameStateData.GameID},
+		CurrentPlayerID: uuid.NullUUID{UUID: gameStateData.CurrentPlayerID},
+		ForbiddenColor:  gameStateData.ForbiddenColor,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	resultGameState := s.DBToModel(ctx, gameState)
+
+	return &resultGameState, nil
 }
