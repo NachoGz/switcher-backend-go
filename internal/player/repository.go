6634d8d7507d3d497344443142acffc7c31c1ac9
NachoGz <ignaciogomezf1@gmail.com>
2025-03-10 12:27:15 -0300

    add GetPlayers and AssignTurnPlayer to repository

--- internal/player/repository.go
+++ internal/player/repository.go
@@ -28,3 +28,13 @@ func (r *PostgresPlayerRepository) CreatePlayer(ctx context.Context, params data
 func (r *PostgresPlayerRepository) CountPlayers(ctx context.Context, gameID uuid.NullUUID) (int64, error) {
 	return r.queries.CountPlayers(ctx, gameID)
 }
+
+// GetPlayers fetches all the players in a game
+func (r *PostgresPlayerRepository) GetPlayers(ctx context.Context, gameID uuid.NullUUID) ([]database.Player, error) {
+	return r.queries.GetPlayers(ctx, gameID)
+}
+
+// AssignTurnPlayer sets the turn for the given player
+func (r *PostgresPlayerRepository) AssignTurnPlayer(ctx context.Context, params database.AssignTurnPlayerParams) error {
+	return r.queries.AssignTurnPlayer(ctx, params)
+}
