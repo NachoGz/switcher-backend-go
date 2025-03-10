9b975197863e29ba57612e615ccdfbc274545584
NachoGz <ignaciogomezf1@gmail.com>
2025-03-10 12:28:11 -0300

    add GetPlayers and AssignRandomTurns  add GetPlayers and AssignRandomTurns service functions

--- internal/player/service.go
+++ internal/player/service.go
@@ -3,6 +3,9 @@ package player
 import (
 	"context"
 	"database/sql"
+	"errors"
+	"fmt"
+	"math/rand"
 
 	"github.com/NachoGz/switcher-backend-go/internal/database"
 	"github.com/google/uuid"
@@ -26,7 +29,7 @@ func (s *Service) CreatePlayer(ctx context.Context, playerData Player) (*Player,
 	player, err := s.playerRepo.CreatePlayer(ctx, database.CreatePlayerParams{
 		ID:          playerData.ID,
 		Name:        playerData.Name,
-		Turn:        sql.NullString{String: playerData.Turn},
+		Turn:        sql.NullString{String: string(playerData.Turn)},
 		GameID:      uuid.NullUUID{UUID: playerData.GameID},
 		GameStateID: uuid.NullUUID{UUID: playerData.GameStateID},
 		Host:        playerData.Host,
@@ -38,4 +41,73 @@ func (s *Service) CreatePlayer(ctx context.Context, playerData Player) (*Player,
 	resultPlayer := s.DBToModel(ctx, player)
 
 	return &resultPlayer, nil
+}
+
+func (s *Service) GetPlayers(ctx context.Context, gameID uuid.NullUUID) ([]Player, error) {
+	players, err := s.playerRepo.GetPlayers(ctx, gameID)
+	if err != nil {
+		return nil, err
+	}
+
+	returnPlayers := []Player{}
+	for _, player := range players {
+		returnPlayers = append(returnPlayers, s.DBToModel(ctx, player))
+	}
+
+	return returnPlayers, nil
+}
+
+// Assign randomly the turns for the players in the game and returrns the id of the first player
+func (s *Service) AssignRandomTurns(ctx context.Context, players []Player) (uuid.UUID, error) {
+	n := len(players)
+	if n == 0 {
+		return uuid.Nil, errors.New("there are no players")
+	}
+
+	randomTurns := make([]int, n)
+	for i := 0; i < n; i++ {
+		randomTurns[i] = i + 1
+	}
+
+	// Shuffle to randomize order
+	rand.Shuffle(n, func(i, j int) {
+		randomTurns[i], randomTurns[j] = randomTurns[j], randomTurns[i]
+	})
+
+	// Define mapping from turn number to turn enum
+	turnMapping := map[int]TurnEnum{
+		1: FIRST,
+		2: SECOND,
+		3: THIRD,
+		4: FOURTH,
+	}
+
+	var firstPlayer *Player
+
+	// Iterate players and assign random turns
+	for i, player := range players {
+		turn := randomTurns[i]
+		turnEnumVal, ok := turnMapping[turn]
+		if !ok {
+			return uuid.Nil, fmt.Errorf("invalid turn: %d", turn)
+		}
+
+		// Assign turn
+		if err := s.playerRepo.AssignTurnPlayer(context.Background(), database.AssignTurnPlayerParams{
+			ID:   player.ID,
+			Turn: sql.NullString{String: string(turnEnumVal)},
+		}); err != nil {
+			return uuid.Nil, err
+		}
+
+		if turn == 1 {
+			firstPlayer = &players[i]
+		}
+	}
+
+	if firstPlayer == nil {
+		return uuid.Nil, errors.New("first player not assigned")
+	}
+
+	return firstPlayer.ID, nil
 }
