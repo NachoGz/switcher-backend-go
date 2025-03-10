0ca4a0324052d2b6bc5c1d76ef9319b267155f0d
NachoGz <ignaciogomezf1@gmail.com>
2025-03-10 12:28:55 -0300

    add UpdateGameState and UpdateCurrentPlayer service functions

--- internal/game_state/service.go
+++ internal/game_state/service.go
@@ -4,27 +4,35 @@ import (
 	"context"
 
 	"github.com/NachoGz/switcher-backend-go/internal/database"
+	"github.com/NachoGz/switcher-backend-go/internal/player"
 	"github.com/google/uuid"
 )
 
 // Service handles all game-related operations
 type Service struct {
 	gameStateRepo GameStateRepository
+	playerRepo    player.PlayerRepository
 }
 
 // NewService creates a new game service
 func NewService(
 	gameStateRepo GameStateRepository,
+	playerRepo player.PlayerRepository,
+
 ) *Service {
 	return &Service{
 		gameStateRepo: gameStateRepo,
+		playerRepo:    playerRepo,
 	}
 }
+
+// Ensure Service implements GameStateService
+var _ GameStateService = (*Service)(nil)
 
 func (s *Service) CreateGameState(ctx context.Context, gameStateData GameState) (*GameState, error) {
 	gameState, err := s.gameStateRepo.CreateGameState(ctx, database.CreateGameStateParams{
 		ID:              gameStateData.ID,
-		State:           gameStateData.State,
+		State:           string(gameStateData.State),
 		GameID:          uuid.NullUUID{UUID: gameStateData.GameID},
 		CurrentPlayerID: uuid.NullUUID{UUID: gameStateData.CurrentPlayerID},
 		ForbiddenColor:  gameStateData.ForbiddenColor,
@@ -36,4 +44,28 @@ func (s *Service) CreateGameState(ctx context.Context, gameStateData GameState) 
 	resultGameState := s.DBToModel(ctx, gameState)
 
 	return &resultGameState, nil
+}
+
+func (s *Service) UpdateGameState(ctx context.Context, gameID uuid.UUID, state State) error {
+	err := s.gameStateRepo.UpdateGameState(ctx, database.UpdateGameStateParams{
+		GameID: uuid.NullUUID{UUID: gameID},
+		State:  string(state),
+	})
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (s *Service) UpdateCurrentPlayer(ctx context.Context, gameID uuid.UUID, currentPlayerID uuid.UUID) error {
+	err := s.gameStateRepo.UpdateCurrentPlayer(ctx, database.UpdateCurrentPlayerParams{
+		GameID:          uuid.NullUUID{UUID: gameID},
+		CurrentPlayerID: uuid.NullUUID{UUID: currentPlayerID},
+	})
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
