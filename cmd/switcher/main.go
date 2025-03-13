3f238002d111662e7dccb520e7dfcbd940809928
NachoGz <ignaciogomezf1@gmail.com>
2025-03-13 19:52:19 -0300

    add baord, movement and figure card services and initialize the gameStateHandlers

--- cmd/switcher/main.go
+++ cmd/switcher/main.go
@@ -6,10 +6,13 @@ import (
 	"net/http"
 	"os"
 
+	"github.com/NachoGz/switcher-backend-go/internal/board"
 	"github.com/NachoGz/switcher-backend-go/internal/database"
+	"github.com/NachoGz/switcher-backend-go/internal/figureCard"
 	"github.com/NachoGz/switcher-backend-go/internal/game"
 	gameState "github.com/NachoGz/switcher-backend-go/internal/game_state"
 	"github.com/NachoGz/switcher-backend-go/internal/middleware"
+	"github.com/NachoGz/switcher-backend-go/internal/movementCard"
 	"github.com/NachoGz/switcher-backend-go/internal/player"
 	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
@@ -44,15 +47,21 @@ func main() {
 	gameRepo := game.NewGameRepository(dbQueries)
 	gameStateRepo := gameState.NewGameStateRepository(dbQueries)
 	playerRepo := player.NewPlayerRepository(dbQueries)
+	boardRepo := board.NewBoardRepository(dbQueries)
+	movementCardRepo := movementCard.NewMovementCardRepository(dbQueries)
+	figureCardRepo := figureCard.NewFigureCardRepository(dbQueries)
 
 	// Create services
 	gameStateService := gameState.NewService(gameStateRepo, playerRepo)
 	playerService := player.NewService(playerRepo)
 	gameService := game.NewService(gameRepo, gameStateRepo, playerRepo, gameStateService, playerService)
+	boardService := board.NewService(boardRepo)
+	movementCardService := movementCard.NewService(movementCardRepo, playerRepo)
+	figureCardService := figureCard.NewService(figureCardRepo, playerRepo)
 
 	// Create handlers
 	gameHandlers := game.NewHandlers(gameService)
-	gameStateHandlers := gameState.NewHandlers(gameStateService)
+	gameStateHandlers := gameState.NewHandlers(gameStateService, playerService, boardService, movementCardService, figureCardService)
 
 	// Configure routes
 	mux := http.NewServeMux()
