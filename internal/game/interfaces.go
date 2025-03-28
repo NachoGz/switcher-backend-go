8f3808edc15f2da6e324681708afd07306527229
NachoGz <ignaciogomezf1@gmail.com>
2025-03-28 18:46:49 -0300

    add DeleteGame service and repository interfaces

--- internal/game/interfaces.go
+++ internal/game/interfaces.go
@@ -13,10 +13,12 @@ type GameService interface {
 	CreateGame(ctx context.Context, gameData Game, playerData player.Player) (*Game, *gameState.GameState, *player.Player, error)
 	GetAvailableGames(ctx context.Context, numPlayers int, page int, limit int, name string) ([]Game, int, error)
 	GetGameByID(ctx context.Context, id uuid.UUID) (*Game, error)
+	DeleteGame(ctx context.Context, id uuid.UUID) error
 }
 
 type GameRepository interface {
 	CreateGame(ctx context.Context, params database.CreateGameParams) (database.Game, error)
 	GetAvailableGames(ctx context.Context) ([]database.Game, error)
 	GetGameById(ctx context.Context, id uuid.UUID) (database.Game, error)
+	DeleteGame(ctx context.Context, id uuid.UUID) error
 }
