f12faf456f0a1420e42bf97c3473fe2c223ee9c3
Ignacio Gomez <ignaciogomezf1@gmail.com>
2025-04-13 12:15:00 -0300

    add GetBox, ChangeBoxColors and WithTx to BoardRepository interface and SwapColors and WithTx to BoardService interface

--- internal/board/interfaces.go
+++ internal/board/interfaces.go
@@ -2,6 +2,7 @@ package board
 
 import (
 	"context"
+	"database/sql"
 
 	"github.com/NachoGz/switcher-backend-go/internal/database"
 	"github.com/google/uuid"
@@ -9,10 +10,15 @@ import (
 
 type BoardService interface {
 	ConfigureBoard(ctx context.Context, gameID uuid.UUID) error
+	SwapColors(ctx context.Context, gameID uuid.UUID, posFrom, posTo BoardPosition) error
+	WithTx(tx *sql.Tx) BoardService
 }
 
 type BoardRepository interface {
 	CreateBoard(ctx context.Context, params database.CreateBoardParams) (database.Board, error)
 	GetBoard(ctx context.Context, gameID uuid.UUID) (database.Board, error)
 	AddBoxToBoard(ctx context.Context, params database.AddBoxToBoardParams) (database.Box, error)
+	GetBox(ctx context.Context, params database.GetBoxParams) (database.Box, error)
+	ChangeBoxColor(ctx context.Context, params database.ChangeBoxColorParams) error
+	WithTx(tx *sql.Tx) BoardRepository
 }
