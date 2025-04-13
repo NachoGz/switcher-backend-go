89e5c7d02cdc4377557eaa525ad2e3968afc9e4c
Ignacio Gomez <ignaciogomezf1@gmail.com>
2025-04-13 19:06:42 -0300

    removed transaction from service logic and change boardService dependancy to boardRepository

--- internal/partialMovements/service.go
+++ internal/partialMovements/service.go
@@ -12,18 +12,17 @@ import (
 
 // Service handles all partial movements operations
 type Service struct {
-	db               *sql.DB
 	partialMovRepo   PartialMovementRepository
-	boardService     board.BoardService
+	boardRepo        board.BoardRepository
 	movementCardRepo movementCard.MovementCardRepository
 }
 
 // NewService creates a new partial movements servzicez
-func NewService(partialMovRepo PartialMovementRepository, boardService board.BoardService,
+func NewService(partialMovRepo PartialMovementRepository, boardRepo board.BoardRepository,
 	movementCardRepo movementCard.MovementCardRepository) PartialMovementService {
 	return &Service{
 		partialMovRepo:   partialMovRepo,
-		boardService:     boardService,
+		boardRepo:        boardRepo,
 		movementCardRepo: movementCardRepo,
 	}
 }
@@ -31,14 +30,6 @@ func NewService(partialMovRepo PartialMovementRepository, boardService board.Boa
 var _ PartialMovementService = (*Service)(nil)
 
 func (s *Service) RevertPartialMovements(ctx context.Context, db *sql.DB, gameID uuid.UUID, playerID uuid.UUID) error {
-	tx, err := s.db.BeginTx(ctx, nil)
-	if err != nil {
-		return err
-	}
-	defer tx.Rollback()
-
-	boardServiceTx := s.boardService.WithTx(tx)
-
 	partialMovements, err := s.partialMovRepo.GetPartialMovementsByPlayer(ctx, database.GetPartialMovementsByPlayerParams{
 		GameID:   gameID,
 		PlayerID: playerID,
@@ -52,16 +43,20 @@ func (s *Service) RevertPartialMovements(ctx context.Context, db *sql.DB, gameID
 		posFrom := board.BoardPosition{PosX: int(movement.PosFromX), PosY: int(movement.PosFromY)}
 		posTo := board.BoardPosition{PosX: int(movement.PosToX), PosY: int(movement.PosToY)}
 
-		if err := boardServiceTx.SwapColors(ctx, gameID, posFrom, posTo); err != nil {
+		if err := s.boardRepo.SwapColors(ctx, gameID, posFrom, posTo); err != nil {
 			return err
 		}
 
 		// Mark card as not used
-		s.movementCardRepo.MarkCardInPlayerHand(ctx, movement.MovementCardID)
+		if err = s.movementCardRepo.MarkCardInPlayerHand(ctx, movement.MovementCardID); err != nil {
+			return err
+		}
 
 		// Delete partial movement
-		s.partialMovRepo.UndoMovementByID(ctx, movement.ID)
+		if err = s.partialMovRepo.UndoMovementByID(ctx, movement.ID); err != nil {
+			return err
+		}
 	}
 
-	return tx.Commit()
+	return nil
 }
